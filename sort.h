6c9b63fcbe49fe3e116da7596dc4a0ca6acdb2e5
Makoena20 <koenaletseku@gmail.com>
2024-03-06 01:51:01 -0800

    100-shell_sort.c

--- sort.h
+++ sort.h
@@ -34,5 +34,8 @@ void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+void print_array(const int *array, size_t size);
+void shell_sort(int *array, size_t size);
+
 #endif /* SORT_H */
 
